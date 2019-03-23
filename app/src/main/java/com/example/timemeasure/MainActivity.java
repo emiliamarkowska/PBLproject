bb0b826100eae9eaad1e16165b61a1ec362c7213
emiliamarkowska <emarkowska0@onet.pl>
2019-03-23 15:19:57 +0100

    small app

--- app/src/main/java/com/example/timemeasure/MainActivity.java
+++ app/src/main/java/com/example/timemeasure/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.timemeasure;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView;
+        final EditText editText;
+        Button button;
+
+        textView = findViewById(R.id.textView2);
+        editText = findViewById(R.id.editText2);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                textView.setText(text);
+            }
+        });
     }
 }
