394e54893f7db18d78a89e8ad3569954810fe41c
emiliamarkowska <emarkowska0@onet.pl>
2019-03-30 21:53:24 +0100

    mam znowu bazę i usera

--- app/src/main/java/com/example/timemeasure/DataBaseHelper.java
+++ app/src/main/java/com/example/timemeasure/DataBaseHelper.java
@@ -1,19 +1,31 @@
 package com.example.timemeasure;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
+
     public static final String databaseName = "dataBaseTimeMeasure";
     public static final int version = 1;
-
     // NAMES OF TABLE AND COLUMNS OF USERS AND INFORMATION ABOUT THEM
     public static final String tableUsersName = "usersTable";
     public static final String userID = "userID";
     public static final String userAge = "userAge";
     public static final String userStudentOrWorker = "StudentOrWorker";
     public static final String userTimeOfUsing = "TimeOfUsing";
+    public static final String userTimeAtSchoolOrWorkPerWeek = "TimeAtSchoolOrWorkPerWeek";
+    public static final String userTimeOnHobbyPerWeek = "TimeOnHobbyPerWeek";
+    public static final String userTimeOnTravellingPerWeek = "TimeOnTravellingPerWeek";
+
+    // NAMES OF TABLE AND COLUMNS OF USERS AND APPLICATIONS THEY USE
+    public static final String tableUsersAndAppsName = "usersAndAppsTable";
+    public static final String userAndAppID = "userAndAppID";
+    public static final String userID1 = "userID";
+    public static final String appName = "appName";
+    public static final String timeOfUsing = "timeOfUsing";
 
 
     public DataBaseHelper(Context context) {
@@ -29,14 +41,53 @@ public class DataBaseHelper extends SQLiteOpenHelper {
                 userID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 userAge + " INTEGER NOT NULL, " +
                 userStudentOrWorker + " TEXT NOT NULL, " +
-                userTimeOfUsing + " TIME NOT NULL" +
-                        ");";
+                userTimeOfUsing + " DATE NOT NULL, " +
+                userTimeAtSchoolOrWorkPerWeek + " DECIMAL NOT NULL, " +
+                userTimeOnHobbyPerWeek + " DECIMAL NOT NULL, " +
+                userTimeOnTravellingPerWeek + " DECIMAL NOT NULL" +
+                ");";
 
         db.execSQL(createUsersTable);
+
+        // CREATE USERS AND APPS RELATION TABLE
+        final String createUsersAndAppsTable = "CREATE TABLE " +
+                tableUsersAndAppsName + "( " +
+                userAndAppID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                userID1 + " INTEGER NOT NULL, " +
+                appName + " TEXT NOT NULL, " +
+                timeOfUsing + " TIME NOT NULL " +
+                ");";
+
+        db.execSQL(createUsersAndAppsTable);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+                db.execSQL("DROP TABLE IF EXISTS " + tableUsersName);
+                db.execSQL("DROP TABLE IF EXISTS " + tableUsersAndAppsName);
+                onCreate(db);
+    }
 
+    public void addInformationAboutUser(User user)
+    {
+        ContentValues cv = new ContentValues();
+        cv.put(userAge, user.getAge());
+        cv.put(userStudentOrWorker, user.getStudentOrWorker());
+        cv.put(userTimeOfUsing, user.getDate());
+        cv.put(userTimeAtSchoolOrWorkPerWeek, user.getTimeAtSchool());
+        cv.put(userTimeOnHobbyPerWeek, user.getTimeOnHobby());
+        cv.put(userTimeOnTravellingPerWeek, user.getTimeOnTravel());
+        SQLiteDatabase db = this.getReadableDatabase();
+        db.insert(tableUsersName, null, cv);
+    }
+
+    public boolean isEmpty()
+    {
+        boolean isEmpt = true;
+        String selectQuery = "SELECT * FROM " + tableUsersName;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.getCount() > 0) isEmpt = false;
+        return isEmpt;
     }
 }
---
+++ app/src/main/java/com/example/timemeasure/User.java
@@ -0,0 +1,87 @@
+package com.example.timemeasure;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+    public class User {
+        private int ID;
+        private int age;
+        private String studentOrWorker;
+        private String date;
+        private double timeAtSchool;
+        private double timeOnHobby;
+        private double timeOnTravel;
+
+
+        public User(int age, double timeAtSchool, double timeOnHobby, double timeOnTravel)
+        {
+            this.ID = 0;
+            this.age = age;
+            this.timeAtSchool = timeAtSchool;
+            this.timeOnHobby = timeOnHobby;
+            this.timeOnTravel = timeOnTravel;
+
+            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+            Date date = new Date();
+
+            this.date = dateFormat.format(date);
+        }
+
+        public int getID() {
+            return ID;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public String getStudentOrWorker() {
+            return studentOrWorker;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public double getTimeAtSchool() {
+            return timeAtSchool;
+        }
+
+        public double getTimeOnHobby() {
+            return timeOnHobby;
+        }
+
+        public double getTimeOnTravel() {
+            return timeOnTravel;
+        }
+
+        public void setID(int ID) {
+            this.ID = ID;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public void setStudentOrWorker(String studentOrWorker) {
+            this.studentOrWorker = studentOrWorker;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public void setTimeAtSchool(double timeAtSchool) {
+            this.timeAtSchool = timeAtSchool;
+        }
+
+        public void setTimeOnHobby(double timeOnHobby) {
+            this.timeOnHobby = timeOnHobby;
+        }
+
+        public void setTimeOnTravel(double timeOnTravel) {
+            this.timeOnTravel = timeOnTravel;
+        }
+    }
+
